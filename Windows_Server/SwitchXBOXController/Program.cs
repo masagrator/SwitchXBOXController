dd0bbb061409fa5cd2a304a71d33e71212c769d1
MasaGratoR <masagrator@gmail.com>
2026-06-25 13:24:07 +0200

    Add files via upload

--- Windows_Server/SwitchXBOXController/Program.cs
+++ Windows_Server/SwitchXBOXController/Program.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
-using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -17,15 +16,7 @@ namespace SwitchXBOXController
         private static X360Controller controller;
 
         private static Thread networkThread;
-        private static Thread displayThread;
         private static UdpClient udpSocket;
-
-        static bool running = true;
-
-        [DllImport("Kernel32")]
-        private static extern bool SetConsoleCtrlHandler(EventHandler handler, bool add);
-        private delegate bool EventHandler();
-        static EventHandler _handler;
 
         public static void networking()
         {
@@ -34,8 +25,6 @@ namespace SwitchXBOXController
             udpSocket = new UdpClient(ipep);
 
             var sender = new IPEndPoint(IPAddress.Any, 0);
-
-            Console.WriteLine();
 
             while (true)
             {
@@ -73,6 +62,7 @@ namespace SwitchXBOXController
                         controller.RightStickX = (short)((data[1] << 8) | data[2]);
                         controller.RightStickY = (short)((data[3] << 8) | data[4]);
                     }
+
                 }
                 catch (SocketException e)
                 {
@@ -81,62 +71,17 @@ namespace SwitchXBOXController
             }
         }
 
-        static void displayButtons()
-        {
-            while(running)
-            {
-                for (int i = 0; i < 150; i++)
-                    Console.Write("\r");
-                Console.Write("|{0}|{1}|{2}|{3}|{4}|{5}|{6}|{7}|{8}|{9}|{10}|{11}|{12}|{13}|{14}|{15}|{16}|{17}|{18}|{19}|",
-                    (controller.Buttons & X360Buttons.A) > 0                ? "A"           : " ",
-                    (controller.Buttons & X360Buttons.B) > 0                ? "B"           : " ",
-                    (controller.Buttons & X360Buttons.X) > 0                ? "X"           : " ",
-                    (controller.Buttons & X360Buttons.Y) > 0                ? "Y"           : " ",
-                    (controller.Buttons & X360Buttons.LeftBumper) > 0       ? "L"           : " ",
-                    (controller.Buttons & X360Buttons.RightBumper) > 0      ? "R"           : " ",
-                    (controller.LeftTrigger > 0)                            ? "ZL"          : "  ",
-                    (controller.RightTrigger > 0)                           ? "ZR"          : "  ",
-                    (controller.Buttons & X360Buttons.Up) > 0               ? "UP"          : "  ",
-                    (controller.Buttons & X360Buttons.Down) > 0             ? "DOWN"        : "    ",
-                    (controller.Buttons & X360Buttons.Left) > 0             ? "LEFT"        : "    ",
-                    (controller.Buttons & X360Buttons.Right) > 0            ? "RIGHT"       : "     ",
-                    (controller.Buttons & X360Buttons.LeftStick) > 0        ? "LSTICK"      : "      ",
-                    (controller.Buttons & X360Buttons.RightStick) > 0       ? "RSTICK"      : "      ",
-                    (controller.Buttons & X360Buttons.Start) > 0            ? "START"       :  "     ",
-                    (controller.Buttons & X360Buttons.Back) > 0             ? "BACK"        : "    ",
-                    "LX " + string.Format("{0:+0.000;-#0.000}", controller.LeftStickX / 32768.0F),
-                    "LY " + string.Format("{0:+0.000;-#0.000}", controller.LeftStickY / 32768.0F),
-                    "RX " + string.Format("{0:+0.000;-#0.000}", controller.RightStickX / 32768.0F),
-                    "RY " + string.Format("{0:+0.000;-#0.000}", controller.RightStickY / 32768.0F));
-            }
-        }
-
-        private static bool onExitHandler()
-        {
-            running = false;
-            scpBus.Unplug(1);
-            udpSocket.Close();
-            networkThread.Join();
-            displayThread.Join();
-
-            return false;
-        }
-
         static void Main(string[] args)
         {
             Console.WriteLine("|-------------------------------|");
             Console.WriteLine("| Switch XBOX Controller Server |");
             Console.WriteLine("|-------------------------------|");
             Console.WriteLine();
+            Console.WriteLine("Running... Please type \"quit\" to close the program.");
 
             System.Timers.Timer timer;
 
-            _handler += new EventHandler(onExitHandler);
-            SetConsoleCtrlHandler(_handler, true);
-
-            detectSwitch();
-
-            Console.WriteLine("Running...");
+            bool running = true;
 
             controller = new X360Controller();
             scpBus = new ScpBus();
@@ -148,50 +93,15 @@ namespace SwitchXBOXController
             timer.Start();
 
             networkThread = new Thread(() => networking());
-            displayThread = new Thread(() => displayButtons());
 
             networkThread.Start();
-            displayThread.Start();
 
-            Console.ReadLine();
+            while (Console.ReadLine() != "quit") ;
+
+            running = false;
+            scpBus.Unplug(1);
+            udpSocket.Close();
+            networkThread.Join();
         }
-
-        private static void detectSwitch()
-        {
-            byte[] buffer = new byte[1024];
-            var epLocal = new IPEndPoint(IPAddress.Any, 8192);
-            EndPoint epRemote = new IPEndPoint(IPAddress.Any, 0);
-            var sock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-            sock.EnableBroadcast = true;   //new code
-            sock.Bind(epLocal);
-
-            while (true)
-            {
-                sock.ReceiveFrom(buffer, ref epRemote);
-                if (Encoding.ASCII.GetString(buffer).Trim().Contains("xbox_switch"))
-                {
-                    sock.SendTo(Encoding.ASCII.GetBytes("xbox\0"), (IPEndPoint)epRemote);
-                    Console.WriteLine($"Connected to: { ((IPEndPoint)epRemote).Address }");
-                    break;
-                }
-            }
-
-            sock.Close();
-        }
-
-        private static IPAddress LocalIPAddress()
-        {
-            if (!System.Net.NetworkInformation.NetworkInterface.GetIsNetworkAvailable())
-            {
-                return null;
-            }
-
-            IPHostEntry host = Dns.GetHostEntry(Dns.GetHostName());
-
-            return host
-                .AddressList
-                .FirstOrDefault(ip => ip.AddressFamily == AddressFamily.InterNetwork);
-        }
-
     }
 }
